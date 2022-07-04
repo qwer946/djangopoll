b3e57662f8c282053ee48991a439b390a0af2039
qwer946 <khalid.ali.987@gmail.com>
2022-07-03 20:00:36 -0500

    finished poll p6

---
+++ poll/views.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'poll/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    ...
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'poll/results.html'
+
+
+def vote(request, question_id):
+    ...  # same as above, no changes needed.
